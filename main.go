e99298638a0367885cab0313890dd5b317cecc6d
Connor Demille <subtlepseudonym@gmail.com>
2023-03-08 21:53:22 -0500

    Rewrite ledger POC as plaid2csv converter
    The work of the ledger POC is done much better by John Wiegley's ledger
    program. This new code uses the plaid API to aggregate transaction data
    from accounts registered with plaid and output a csv that's ingestible
    by ledger or another csv2ledger style tool

--- main.go
+++ main.go
@@ -1,66 +1,233 @@
-package ledger
+package main
 
-import "time"
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
-// LineItem is any item that has monetary value. Additional getters
-// are included so that it can be identified and described
-type LineItem interface {
-	ID() string // unique
-	Name() string
-	Description() string // FIXME: optional? that's more of a db concern
-	Value() int // in cents
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string            `yaml:"environment"`
+	ClientID    string            `yaml:"client_id"`
+	Secret      string            `yaml:"secret"`
+	Token       string            `yaml:"token"`
+	Accounts    map[string]string `yaml:"accounts"`
 }
 
-// Expense is a basic LineItem that includes the date when the
-// expense occurred
-type Expense struct {
-	id          string
-	name        string
-	description string
-	value       int
+const (
+	defaultTransactionCount  = 500 // max value
+	defaultCategoryDelimiter = "."
 
-	Date time.Time // date of charge
+	defaultDateFormat   = "2006-01-02"
+	defaultAmountFormat = "%0.2f"
+
+	plaidDomain          = "plaid.com"
+	transactionsEndpoint = "transactions/get"
+)
+
+var (
+	configPath string
+	outputPath string
+
+	ignorePending     bool
+	transactionCount  int
+	categoryDelimiter string
+
+	postDateFormat string
+	authDateFormat string
+	amountFormat   string
+)
+
+func main() {
+	// required
+	flag.StringVar(&configPath, "config", "config.yaml", "Config file path")
+	flag.StringVar(&outputPath, "output", "transactions.csv", "Path for output file")
+	startDate := flag.String("start", "", "Start date, inclusive. Format: YYYY-MM-DD")
+	endDate := flag.String("end", "", "End date, inclusive. Format: YYYY-MM-DD")
+
+	// optional
+	flag.BoolVar(&ignorePending, "ignore-pending", false, "Omit pending transactions")
+	flag.IntVar(&transactionCount, "count", defaultTransactionCount, "Number of transactions to request, 0-500")
+	flag.StringVar(&categoryDelimiter, "category-delimiter", defaultCategoryDelimiter, "Delimiter for joining category hierarchy")
+
+	flag.StringVar(&postDateFormat, "format-post-date", defaultDateFormat, "Output format for transaction post date")
+	flag.StringVar(&authDateFormat, "format-auth-date", defaultDateFormat, "Output format for transaction authorization date")
+	flag.StringVar(&amountFormat, "format-amount", defaultAmountFormat, "Output format for amount")
+	flag.Parse()
+
+	start, err := time.Parse(time.DateOnly, *startDate)
+	if err != nil {
+		log.Printf("Error parsing start date: %s\n", err)
+		return
+	}
+
+	end, err := time.Parse(time.DateOnly, *endDate)
+	if err != nil {
+		log.Printf("Error parsing end date: %s\n", err)
+		return
+	}
+
+	f, err := os.Open(configPath)
+	if err != nil {
+		log.Printf("Error opening config file: %s\n", err)
+		return
+	}
+	defer f.Close()
+
+	var config Config
+	err = yaml.NewDecoder(f).Decode(&config)
+	if err != nil {
+		log.Printf("Error decoding config file: %s\n", err)
+		return
+	}
+
+	outputFile, err := os.OpenFile(outputPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Error opening output file for writing: %s\n", err)
+		return
+	}
+	defer outputFile.Close()
+
+	response, err := RequestTransactions(&config, start, end, transactionCount, 0)
+	if err != nil {
+		log.Printf("Error requesting transactions from plaid: %s\n", err)
+		return
+	}
+
+	output := csv.NewWriter(outputFile)
+	headers := []string{
+		"Post Date",
+		"Authorized Date",
+		"Account",
+		"Check Number",
+		"Payee",
+		"Amount",
+		"Currency",
+		"Category",
+		"Transaction ID",
+	}
+	output.Write(headers)
+	err = WriteTransactions(output, response, &config)
+	if err != nil {
+		log.Printf("Error writing transactions to output: %s\n", err)
+		return
+	}
+
+	responseTotal := response.Total
+	for response.Total >= transactionCount {
+		response, err = RequestTransactions(&config, start, end, transactionCount, responseTotal)
+		if err != nil {
+			log.Printf("Error requesting transactions from plaid: %s\n", err)
+			return
+		}
+
+		err = WriteTransactions(output, response, &config)
+		if err != nil {
+			log.Printf("Error writing transactions to output: %s\n", err)
+			return
+		}
+	}
 }
 
-// BudgetedExpense is an Expense that draws funds from a budget
-// rather than income
-// FIXME: the concepts of `income` and `budget` are not yet rigorously defined
-type BudgetedExpense struct {
-	Expense
-	Budget *Budget // budget from which expense draws funds
+func RequestTransactions(config *Config, start, end time.Time, count, offset int) (*TransactionsResponse, error) {
+	accounts := make([]string, 0, len(config.Accounts))
+	for id := range config.Accounts {
+		accounts = append(accounts, id)
+	}
+
+	request := &TransactionsRequest{
+		ClientID:    config.ClientID,
+		Secret:      config.Secret,
+		AccessToken: config.Token,
+		StartDate:   start.Format(time.DateOnly),
+		EndDate:     end.Format(time.DateOnly),
+		Options: TransactionsRequestOptions{
+			Count:                      count,
+			Offset:                     offset,
+			AccountIDs:                 accounts,
+			IncludeOriginalDescription: true,
+		},
+	}
+
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(request)
+	if err != nil {
+		return nil, fmt.Errorf("marshal request: %w", err)
+	}
+
+	url := fmt.Sprintf("https://%s.%s/%s", config.Environment, plaidDomain, transactionsEndpoint)
+	req, err := http.NewRequest(http.MethodPost, url, &body)
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+	req.Header.Add("content-type", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("do request: %w", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad response: %s", res.Status)
+	}
+
+	var response TransactionsResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("decode response: %w", err)
+	}
+
+	if rerr := response.Item.Error; rerr.Type != "" {
+		return &response, fmt.Errorf("response error: %s %s %s", rerr.Type, rerr.Code, rerr.Message)
+	}
+
+	return &response, nil
 }
 
-// Category is a nestable LineItem that contains other LineItem structs
-type Category interface {
-	LineItem
-	Items() []LineItem
-	AddItem(LineItem)
-}
+func WriteTransactions(output *csv.Writer, response *TransactionsResponse, config *Config) error {
+	for _, transaction := range response.Transactions {
+		if ignorePending && transaction.Pending {
+			continue
+		}
 
-// Budget is a Category that can provide funds for BudgetedExpense structs
-type Budget struct {
-	id          string
-	name        string
-	description string
-	total       int
-	items       []LineItem
-}
+		payee := transaction.Name
+		if transaction.MerchantName != "" {
+			payee = transaction.MerchantName
+		}
 
-func (b *Budget) AddItem(item LineItem) {
-	b.items = append(b.items, item)
-	b.total += item.Value()
-}
+		currency := transaction.ISOCurrency
+		if transaction.UnofficialCurrency != "" {
+			currency = transaction.UnofficialCurrency
+		}
 
-func (b *Budget) Items() []LineItem {
-	return b.items
-}
+		output.Write([]string{
+			transaction.Date.Format(postDateFormat),
+			transaction.AuthorizedDate.Format(authDateFormat),
+			config.Accounts[transaction.AccountID],
+			transaction.CheckNumber,
+			payee,
+			fmt.Sprintf(amountFormat, transaction.Amount),
+			currency,
+			strings.Join(transaction.Category, categoryDelimiter),
+			transaction.ID,
+		})
+		if err := output.Error(); err != nil {
+			return fmt.Errorf("write record: %w", err)
+		}
+	}
 
-// Ledger should have specific fields for top-level concerns
-// general health of finances, value remaining in budgets,
-// total cost by category, etc
-type Ledger struct {
-	Income   Category
-	// FIXME: may want Expenses and Budgets to be map[string]Object
-	Expenses []Category
-	Budgets  []Budget
+	output.Flush()
+	if err := output.Error(); err != nil {
+		return fmt.Errorf("flush output: %w", err)
+	}
+
+	return nil
 }
---
+++ plaid.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type TransactionsRequest struct {
+	ClientID    string                     `json:"client_id"`
+	Secret      string                     `json:"secret"`
+	AccessToken string                     `json:"access_token"`
+	StartDate   string                     `json:"start_date"`
+	EndDate     string                     `json:"end_date"`
+	Options     TransactionsRequestOptions `json:"options"`
+}
+
+type TransactionsRequestOptions struct {
+	Count                      int      `json:"count"` // max 500
+	Offset                     int      `json:"offset"`
+	AccountIDs                 []string `json:"account_ids"`
+	IncludeOriginalDescription bool     `json:"include_original_description"`
+}
+
+type TransactionsResponse struct {
+	Item         Item          `json:"item"`
+	Accounts     []Account     `json:"accounts"`
+	Transactions []Transaction `json:"transactions"`
+	RequestID    string        `json:"request_id"`
+	Total        int           `json:"total_transactions"`
+}
+
+type Item struct {
+	ID            string `json:"item_id"`
+	InstitutionID string `json:"institution_id"`
+
+	AvailableProducts []string `json:"available_products"`
+	BilledProducts    []string `json:"billed_products"`
+	OptionalProducts  []string `json:"optional_products"`
+	Products          []string `json:"products"`
+
+	UpdateType string   `json:"update_type"`
+	Error      APIError `json:"error"`
+}
+
+type APIError struct {
+	Type             string     `json:"error_type"`
+	Code             string     `json:"error_code"`
+	Message          string     `json:"error_message"`
+	Display          string     `json:"display_message"`
+	RequestID        string     `json:"request_id"`
+	Causes           []APIError `json:"causes"`
+	HTTPStatus       int        `json:"status"`
+	DocumentationURL string     `json:"documentation_url"`
+}
+
+type Account struct {
+	ID           string  `json:"account_id"`
+	Balance      Balance `json:"balances"`
+	Mask         string  `json:"mask"`
+	Name         string  `json:"name"`
+	OfficialName string  `json:"official_name"`
+	Type         string  `json:"type"`
+	Subtype      string  `json:"subtype"`
+}
+
+type Balance struct {
+	Available          float64 `json:"available"`
+	Current            float64 `json:"current"`
+	Limit              float64 `json:"limit"`
+	ISOCurrency        string  `json:"iso_currency_code"`
+	UnofficialCurrency string  `json:"unofficial_currency_code"`
+}
+
+type Transaction struct {
+	ID           string `json:"transaction_id"`
+	Type         string `json:"transaction_type"`
+	AccountID    string `json:"account_id"`
+	AccountOwner string `json:"account_owner"`
+
+	Amount             float64 `json:"amount"`
+	ISOCurrency        string  `json:"iso_currency_code"`
+	UnofficialCurrency string  `json:"unofficial_currency_code"`
+	CheckNumber        string  `json:"check_number"`
+
+	CategoryID string   `json:category_id"`
+	Category   []string `json:"category"`
+
+	Date           Date      `json:"date"`
+	Time           time.Time `json:"datetime"`
+	AuthorizedDate Date      `json:"authorized_date"`
+	AuthorizedTime time.Time `json:"authorized_time"`
+	Location       Location  `json:"location"`
+
+	OriginalDescription string      `json:"original_description"`
+	Name                string      `json:"name"`
+	MerchantName        string      `json:"merchant_name"`
+	PaymentMeta         PaymentMeta `json:"payment_meta"`
+	PaymentChannel      string      `json:"payment_channel"`
+
+	Pending              bool   `json:"pending"`
+	PendingTransactionID string `json:"pending_transaction_id"`
+	TransactionCode      string `json:"transaction_code"`
+}
+
+type Date struct {
+	time.Time
+}
+
+func (d Date) Format(layout string) string {
+	if d.Time.IsZero() {
+		return ""
+	}
+	return d.Time.Format(layout)
+}
+
+func (d *Date) UnmarshalJSON(b []byte) error {
+	var tmp string
+	err := json.Unmarshal(b, &tmp)
+	if err != nil {
+		return err
+	}
+
+	if tmp != "" {
+		date, err := time.Parse(time.DateOnly, tmp)
+		if err != nil {
+			return err
+		}
+		d.Time = date
+	}
+	return nil
+}
+
+type Location struct {
+	Address     string  `json:"address"`
+	City        string  `json:"city"`
+	Region      string  `json:"region"`
+	PostalCode  string  `json:"postal_code"`
+	Country     string  `json:"country"`
+	Latitude    float64 `json:"lat"`
+	Longitude   float64 `json:"lon"`
+	StoreNumber string  `json:"store_number"`
+}
+
+type PaymentMeta struct {
+	ReferenceNumber  string `json:"reference_number"`
+	PPDID            string `json:"ppd_id"`
+	Payee            string `json:"payee"`
+	ByOrderOf        string `json:"by_order_of"`
+	Payer            string `json:"payer"`
+	PaymentMethod    string `json:"payment_method"`
+	PaymentProcessor string `json:"payment_processor"`
+	Reason           string `json:"reason"`
+}
